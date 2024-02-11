29b313753a941102ae06cd8cf85de4613ea0b8c8
What-I-pressed <AntonCymbaliuk.oof@gmail.com>
2024-02-11 10:08:30 +0200

    a

--- ServerScript/TCP.cs
+++ ServerScript/TCP.cs
@@ -2,6 +2,7 @@
 using System.Net.Sockets;
 using System.Threading.Tasks;
 using System;
+using System.Net.NetworkInformation;
 
 namespace ServerScript
 {
@@ -13,7 +14,7 @@ namespace ServerScript
 
         public TCP()
         {
-            _tcpListener = new TcpListener(_port);
+            _tcpListener = new TcpListener(new IPEndPoint(IPAddress.Parse("13.36.169.142"), _port));
             _tcpListener.Start();
         }
 
