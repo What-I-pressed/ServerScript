7c9d17575a26f5fb578201fcab20eb99188d22b4
What-I-pressed <AntonCymbaliuk.oof@gmail.com>
2024-02-17 17:37:42 +0200

    bssdbs

--- ServerScript/TCP.cs
+++ ServerScript/TCP.cs
@@ -3,6 +3,7 @@ using System.Net.Sockets;
 using System.Threading.Tasks;
 using System;
 using System.Net.NetworkInformation;
+using System.Text;
 
 namespace ServerScript
 {
@@ -11,6 +12,7 @@ namespace ServerScript
     {
         public TcpListener _tcpListener;
         private int _port = 5518;
+        private event AsyncCallback _incomingMessageHandler;
 
         public TCP()
         {
@@ -25,52 +27,80 @@ namespace ServerScript
 
         public async Task StartListen()
         {
-            Console.WriteLine("Server start listen");
-            while (true)
-            {
-                TcpClient tcpClient = _tcpListener.AcceptTcpClient();
-                Console.WriteLine("New client has been accepted!!");
-            }
+            //Console.WriteLine("Server start listen");
+            //while (true)
+            //{
+            //    TcpClient tcpClient = _tcpListener.AcceptTcpClient();
+            //    Console.WriteLine("New client has been accepted!!");
+            //}
 
-            //_tcpListener.BeginAcceptTcpClient(AcceptClient, null);
+            _tcpListener.BeginAcceptTcpClient(AcceptClient, null);
         }
 
         private void AcceptClient(IAsyncResult res) { 
             IPEndPoint endPoint = new IPEndPoint(IPAddress.Any, _port);
-            try
-            {
-                TcpClient client = _tcpListener.EndAcceptTcpClient(res);
-                Console.WriteLine("Connection accepted");
-                _tcpListener.BeginAcceptTcpClient(AcceptClient, null);
-                NetworkStream nStream = client.GetStream();
-
-                byte[] buffer = new byte[5];
-                nStream.Read(buffer, 0, 5);
-                switch (buffer[0])
-                {
-                    case (byte)RequestType.AddClient:               
-                        ClientManager.AddClient(BitConverter.ToInt32(new byte[] { buffer[1], buffer[2], buffer[3], buffer[4] }, 0),
-                            client.Client.RemoteEndPoint as IPEndPoint);
-                        Console.WriteLine("New client has been added");
-                        break;
-                    case (byte)RequestType.AudioConnectWith:
-                                                                       
-
-                        break;              
-                    case (byte)RequestType.SendTextMessage:
-                        Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
-                        socket.Connect(ClientManager.GetClientIPEndPoint(BitConverter.ToInt32(new byte[] { buffer[1], buffer[2], buffer[3], buffer[4] })));
-                        buffer = new byte[nStream.Length];                                      
-                        nStream.Read(buffer, 0, buffer.Length);
-                        SocketAsyncEventArgs socketAsyncEventArgs = new SocketAsyncEventArgs();
-                        socketAsyncEventArgs.SetBuffer(buffer);
-                        Task.Run(() => socket.SendAsync(socketAsyncEventArgs));
-                        break;
-                }
-            }
-            catch (ArgumentException ex) { Console.WriteLine("Argument exception \n" + ex.Message); }
-            catch (Exception ex) { Console.WriteLine(ex.Message); }
             
+            
+            Task.Run(() => MessageHandler(_tcpListener.EndAcceptTcpClient(res)));
+            _tcpListener.BeginAcceptTcpClient(AcceptClient, null);
+            
+            //byte[] message = new byte[1];
+            //
+            //AsyncCallback handler = async (IAsyncResult result) =>
+            //{
+            //    client.Client.EndReceive(result);
+            //    Console.WriteLine("Send message request was accepted");
+            //    NetworkStream networkStream = client.GetStream();
+            //    message = new byte[networkStream.Length];
+            //    networkStream.Read(message, 0, message.Length);
+            //    SocketAsyncEventArgs asyncEventArgs = new SocketAsyncEventArgs();
+            //    asyncEventArgs.SetBuffer(message);
+            //    client.Client.SendToAsync(asyncEventArgs, ClientManager.GetClientIPEndPoint(BitConverter.ToInt32(new byte[] { message[1], message[2], message[3], message[4] })));
+            //};
+            //_incomingMessageHandler += handler;
+            //_incomingMessageHandler += (IAsyncResult res) => { client.Client.BeginReceive(message, 0, message.Length, SocketFlags.None, _incomingMessageHandler, null); };
+            //client.Client.BeginReceive(message, 0, message.Length, SocketFlags.None, _incomingMessageHandler, null);
+        }
+
+        private async void MessageHandler(TcpClient client)
+        {
+            while (true)
+            {
+                try
+                {
+                    NetworkStream nStream = client.GetStream();
+                    Console.WriteLine("Connection accepted");
+                    byte[] buffer = new byte[5];
+                    nStream.Read(buffer, 0, 5);
+                    switch (buffer[0])
+                    {
+                        case (byte)RequestType.AddClient:
+                            ClientManager.AddClient(BitConverter.ToInt32(new byte[] { buffer[1], buffer[2], buffer[3], buffer[4] }, 0),
+                                client.Client.RemoteEndPoint as IPEndPoint);
+                            Console.WriteLine("New client has been added");
+                            break;
+                        case (byte)RequestType.AudioConnectWith:
+
+
+                            break;
+                        case (byte)RequestType.SendTextMessage:
+
+                            Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+                            socket.Connect(ClientManager.GetClientIPEndPoint(BitConverter.ToInt32(new byte[] { buffer[1], buffer[2], buffer[3], buffer[4] })));
+                            buffer = new byte[nStream.Length];
+                            nStream.Read(buffer, 0, buffer.Length);
+                            SocketAsyncEventArgs socketAsyncEventArgs = new SocketAsyncEventArgs();
+                            socketAsyncEventArgs.SetBuffer(buffer);
+                            Task.Run(() => socket.SendAsync(socketAsyncEventArgs));
+                            Console.WriteLine("Message was forwarded");
+                            break;
+
+                    }
+                }
+                catch (ArgumentException ex) { Console.WriteLine("Argument exception \n" + ex.Message); }
+                catch (Exception ex) { Console.WriteLine(ex.Message); }
+                Thread.Sleep(200);
+            }
         }
     }
 }
